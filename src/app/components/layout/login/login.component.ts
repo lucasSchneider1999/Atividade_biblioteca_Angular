019b036ba47110ee00039f634ec13c38cf55df75
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-13 22:50:18 -0400

    FIX login component

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
-
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [MdbFormsModule, RouterOutlet, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
