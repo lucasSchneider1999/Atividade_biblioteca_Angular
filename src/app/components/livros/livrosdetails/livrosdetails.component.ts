6728f7f94222700c36378cd78e4b970616b4b5d7
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-14 14:43:23 -0400

    ADD novo e editar funcoes

--- src/app/components/livros/livrosdetails/livrosdetails.component.ts
+++ src/app/components/livros/livrosdetails/livrosdetails.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Livro } from '../../../models/livro';
 
@@ -15,9 +15,31 @@ import { Livro } from '../../../models/livro';
 export class LivrosdetailsComponent {
 
   livro: Livro = new Livro(0, "");
+  //servve para pegar um parametro de rota
+  router = inject(ActivatedRoute);
+  //serve para redirecionar para outra rota
+  router2 = inject(Router);
+
+  constructor(){
+    let id = this.router.snapshot.params['id'];
+    if(id > 0){
+      this.findById(id);
+    }
+  }
+
+  findById(id: number){
+    let livroRetornado: Livro = new Livro(id, "editado");
+    this.livro = livroRetornado;
+  }
 
   salvar(){
-    alert("salvo com sucesso");
+    if(this.livro.id > 0){
+      alert("editado com sucesso");
+      this.router2.navigate(['admin/livros'], {state: {livroEditado: this.livro}})
+    }else{
+      alert("salvo com sucesso");
+      this.router2.navigate(['admin/livros'], {state: {livroNovo: this.livro}})
+    }
   }
 
 }
--- src/app/components/livros/livroslist/livroslist.component.ts
+++ src/app/components/livros/livroslist/livroslist.component.ts
@@ -14,10 +14,25 @@ export class LivroslistComponent {
   lista: Livro[] = [];
 
   constructor(){
+
     this.lista.push(new Livro(1, "O iluminado"))
     this.lista.push(new Livro(2, "Mochileiro das galaxias"))
     this.lista.push(new Livro(3, "senhor dos aneis"))
     this.lista.push(new Livro(4, "Os mentirosos"))
+
+    let livroNovo = history.state.livroNovo;
+    let livroEditado = history.state.livroEditado;
+
+    if (livroNovo){
+      livroNovo.id = 999;
+      this.lista.push(livroNovo);
+    }
+
+    if (livroEditado){
+      let index = this.lista.findIndex(x => {return x.id == livroEditado.id});
+      this.lista[index] = livroEditado;
+    }
+
   }
 
   deletar(id:number){
