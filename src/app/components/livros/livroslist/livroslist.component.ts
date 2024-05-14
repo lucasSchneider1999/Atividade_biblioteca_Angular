a9b30596acd26486f937be8b86a84c5e581dd15c
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-13 23:37:47 -0400

    FIX livroslist component

--- src/app/components/livros/livroslist/livroslist.component.ts
+++ src/app/components/livros/livroslist/livroslist.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Livro } from '../../../models/livro';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-livroslist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './livroslist.component.html',
   styleUrl: './livroslist.component.scss'
 })
 export class LivroslistComponent {
+  
+  lista: Livro[] = [];
+
+  constructor(){
+    this.lista.push(new Livro(1, "O iluminado"))
+    this.lista.push(new Livro(2, "Mochileiro das galaxias"))
+    this.lista.push(new Livro(3, "senhor dos aneis"))
+    this.lista.push(new Livro(4, "Os mentirosos"))
+  }
+
+  deletar(){
+
+  }
 
 }
--- src/app/models/livro.ts
+++ src/app/models/livro.ts
@@ -2,5 +2,10 @@ export class Livro {
 
     id!: number;
     nome!: string;
+
+    constructor(id: number, nome: string){
+        this.id = id;
+        this.nome = nome;
+    }
     
 }
