7813712e2c390d044fb479660eb14d0cffda3924
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-13 23:53:34 -0400

    ADD livrosdetails component

--- src/app/components/livros/livrosdetails/livrosdetails.component.ts
+++ src/app/components/livros/livrosdetails/livrosdetails.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Livro } from '../../../models/livro';
 
 @Component({
   selector: 'app-livrosdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, RouterOutlet, FormsModule],
   templateUrl: './livrosdetails.component.html',
   styleUrl: './livrosdetails.component.scss'
 })
+
 export class LivrosdetailsComponent {
+
+  livro: Livro = new Livro(0, "");
+
+  salvar(){
+    alert("salvo com sucesso");
+  }
 
 }
